0b5aef71ad5591cf0babe27814c036d9c34324c9
RayPereiraP <rayanepereira547@gmail.com>
2025-01-07 20:13:01 -0300

    dia 1

--- Days/day1.py
+++ Days/day1.py
@@ -1,1 +1,5 @@
 #gerador de nomes de bandas
+print('Welcome to the Band Name Generator')
+city = input('Which city did you grow up in?\n')
+pet = input('What is the name of a pet?'\n)
+print('Your band name could be: ' + city + ' ' + pet)
